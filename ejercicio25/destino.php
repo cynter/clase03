6755c651f8bc04d4ba3103a8e3594af071643f08
cynter <cynteran27@gmail.com>
2016-08-30 22:10:25 -0300

    ejercicio25-1ra parte

---
+++ ejercicio25/destino.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<link rel="stylesheet" type="text/css" href="animacion.css">
+</head>
+<body>
+	<div class="CajaInicio animated bounceInRight">
+			<h1>La Superficie del rectangulo es:</h1>
+	</div>
+	
+	<div class="CajaInicio animated bounceInRight">
+	
+	</div>
+	
+	
+</body>
+</html>
